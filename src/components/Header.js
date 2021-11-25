a43cd11795b285f7ed311f7cb939482fef91c622
Abraham0912 <09120912ahn@gmail.com>
2021-11-25 10:42:21 -0600

    Header, install ui-icons, add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import TextField from '@mui/material/TextField';
+import Avatar from '@mui/material/Avatar';
 import './Header.css'
 export const Header = () => {
     return (
         <div className="header">
-            <h2>Header.js</h2>
+            <div className="header___left">
+                <MenuIcon color="primary" fontSize="large"/>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png" alt="React.svg.png" />
+                <p>React</p>
+            </div>
+            <div className="header__center">
+               <h3>Home</h3>
+               <h3>Main</h3>
+               <h3>MoreInfo <span><ArrowDropDownIcon/></span></h3>
+            </div>
+            <div className="header__right">
+                <form noValidate autoComplete="off">
+                    <TextField id="filled-basic" label="Filled" variant="filled" />
+                </form>
+                <Avatar/>
+            </div>
         </div>
     )
 }
